22a30c8876ab0690ea57d9645c712ac818cd99c6
Jonathan Burke <jonathan.burke@codecentric.de>
2022-05-10 11:20:51 +0200

    add computer LAN logo
    Signed-off-by: Jonathan Burke <jonathan.burke@codecentric.de>

--- app/sections/Introduction.tsx
+++ app/sections/Introduction.tsx
@@ -13,6 +13,7 @@ import { Link } from "@remix-run/react";
 import dayjs from "dayjs";
 import { Edit } from "tabler-icons-react";
 import { Can } from "~/components";
+import { Computer } from "~/components/Computer";
 import type { getNews } from "~/models/news.server";
 
 export function Introduction({
@@ -20,8 +21,55 @@ export function Introduction({
 }: {
   news: Awaited<ReturnType<typeof getNews>>;
 }) {
+  const width = 17;
+  const active = [
+    // L
+    ...[
+      1 + 1 * width,
+      1 + 2 * width,
+      1 + 3 * width,
+      1 + 4 * width,
+      2 + 4 * width,
+      3 + 4 * width,
+      4 + 4 * width,
+    ],
+    // A
+    ...[
+      6 + 4 * width,
+      6 + 3 * width,
+      7 + 2 * width,
+      8 + 1 * width,
+      9 + 2 * width,
+      10 + 3 * width,
+      10 + 4 * width,
+      7 + 3 * width,
+      8 + 3 * width,
+      9 + 3 * width,
+    ],
+    // N
+    ...[
+      12 + 4 * width,
+      12 + 3 * width,
+      12 + 2 * width,
+      12 + 1 * width,
+      13 + 2 * width,
+      14 + 3 * width,
+      15 + 4 * width,
+      15 + 3 * width,
+      15 + 2 * width,
+      15 + 1 * width,
+    ],
+  ];
+
   return (
     <Container>
+      <Box
+        sx={{ display: "grid", gridTemplateColumns: `repeat(${width}, auto)` }}
+      >
+        {Array.from({ length: 6 * width }, (_, idx) => (
+          <Computer key={idx} active={active.includes(idx)} />
+        ))}
+      </Box>
       <Can I="create" a="News">
         <Button component={Link} to="/admin/news" variant="subtle">
           Eine News schreiben
