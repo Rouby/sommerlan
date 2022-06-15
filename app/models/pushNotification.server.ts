f7915046cedfa324a2dbc8ad261da668a90b3669
Jonathan Burke <jonathan.burke@codecentric.de>
2022-06-15 10:32:47 +0200

    handle expired or unsubscribed push notifications
    Signed-off-by: Jonathan Burke <jonathan.burke@codecentric.de>

--- app/models/pushNotification.server.ts
+++ app/models/pushNotification.server.ts
@@ -2,6 +2,7 @@ import { ForbiddenError } from "@casl/ability";
 import * as webpush from "web-push";
 import { prisma } from "~/db.server";
 import { defineAbilityForUser } from "~/utils/ability.server";
+import { json } from "~/utils/superjson";
 
 export type { User } from "@prisma/client";
 
@@ -29,24 +30,43 @@ export async function sendNotificationsToUsers(
     ...(userIds.length ? { where: { userId: { in: userIds } } } : {}),
   });
 
-  console.log("sending to", subscriptions.length, userIds);
+  console.log("Sending notifications to", subscriptions.length, userIds);
 
+  let success = 0;
   for (const subscription of subscriptions) {
-    await webpush.sendNotification(
-      {
-        endpoint: subscription.endpoint,
-        keys: subscription.keys as any,
-      },
-      JSON.stringify(payload),
-      {
-        gcmAPIKey: process.env.GCM_KEY,
-        vapidDetails: {
-          publicKey: process.env.APPLICATION_SERVER_KEY!,
-          privateKey: process.env.APPLICATION_SERVER_PRIVATE_KEY!,
-          subject: "https://sommerlan.rocks",
+    try {
+      await webpush.sendNotification(
+        {
+          endpoint: subscription.endpoint,
+          keys: subscription.keys as any,
         },
+        JSON.stringify(payload),
+        {
+          gcmAPIKey: process.env.GCM_KEY,
+          vapidDetails: {
+            publicKey: process.env.APPLICATION_SERVER_KEY!,
+            privateKey: process.env.APPLICATION_SERVER_PRIVATE_KEY!,
+            subject: "https://sommerlan.rocks",
+          },
+        }
+      );
+      ++success;
+    } catch (err) {
+      if (err instanceof webpush.WebPushError) {
+        if (err.statusCode === 410) {
+          console.log(
+            "Push notification expired or unsubscribed",
+            subscription.endpoint
+          );
+          await prisma.pushNotification.delete({
+            where: { id: subscription.id },
+          });
+        }
       }
-    );
-    console.log("Sent notification");
+    }
   }
+
+  console.log("Sent notification");
+
+  return json({ success }, { status: 200 });
 }
