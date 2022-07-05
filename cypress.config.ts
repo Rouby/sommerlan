f50f35e10cb8322a7a148be99cb77b72b8064b63
Jonathan Burke <jonathan.burke.1311@googlemail.com>
2022-07-05 08:33:30 +0200

    move cy plugins
    Signed-off-by: Jonathan Burke <jonathan.burke.1311@googlemail.com>

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,10 +2,24 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require("./cypress/plugins/index.ts")(on, config);
+      const isDev = config.watchForFileChanges;
+      const port = process.env.PORT ?? (isDev ? "3000" : "8811");
+      const configOverrides: Partial<Cypress.PluginConfigOptions> = {
+        baseUrl: `http://localhost:${port}`,
+        video: !process.env.CI,
+        screenshotOnRunFailure: !process.env.CI,
+      };
+      Object.assign(config, configOverrides);
+
+      on("task", {
+        log(message) {
+          console.log(message);
+          return null;
+        },
+      });
+
+      return config;
     },
     baseUrl: "http://localhost:3000",
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
