195021622babeb8a04e5ab91b4dbcc339fb32871
Jonathan Burke <jonathan.burke@codecentric.de>
2023-07-11 11:37:28 +0200

    fix: use correct env ids
    Signed-off-by: Jonathan Burke <jonathan.burke@codecentric.de>

--- packages/server/src/env.ts
+++ packages/server/src/env.ts
@@ -1,8 +1,8 @@
 export const expectedOrigin = process.env.APP_ORIGIN ?? "http://localhost:5173";
 
 const originToEnv = {
-  "https://sommerlan.de": "production",
-  "https://staging.sommerlan.de": "staging",
+  "https://sommerlan.rocks": "production",
+  "https://staging.sommerlan.rocks": "staging",
 } as const;
 
 export function getEnv() {
