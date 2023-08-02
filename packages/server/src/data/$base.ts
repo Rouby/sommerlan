4d17b44787e04f61e158f3d2f8cea0c3a57f9747
Jonathan Burke <jonathan.burke.1311@googlemail.com>
2023-08-02 23:23:00 +0200

    fix: improve caching and ensure cache is populated before mutating
    Signed-off-by: Jonathan Burke <jonathan.burke.1311@googlemail.com>

--- packages/server/src/data/$base.ts
+++ packages/server/src/data/$base.ts
@@ -1,7 +1,4 @@
-import { Operation, applyPatch, compare } from "fast-json-patch";
-import newrelic from "newrelic";
-import { logger } from "../logger";
-import { getSheet } from "./$api";
+import { allRows, deleteRow, updateRow } from "./$cache";
 
 export type Values<T> = Partial<
   // eslint-disable-next-line @typescript-eslint/ban-types
@@ -10,8 +7,6 @@ export type Values<T> = Partial<
 
 type StaticThis<T extends Base> = { new (): T };
 
-const deleteMarker = Symbol.for("delete");
-
 export abstract class Base {
   static sheetName: string;
   abstract get sheetName(): string;
@@ -19,74 +14,21 @@ export abstract class Base {
   public id = "";
 
   async save() {
-    const cachedValues = cache
-      .get(this.sheetName)
-      ?.find((values) => values.id === this.id);
-
-    const thisValues = Object.fromEntries(Object.entries(this));
-
-    const diff = compare(cachedValues ?? {}, thisValues, true);
-
-    logger.debug(
-      { diff, cachedValues, thisValues, sheetName: this.sheetName },
-      "Storing diff"
-    );
-
     const thisClass = this.constructor as StaticThis<Base>;
 
-    if (!patches.has(thisClass)) {
-      patches.set(thisClass, new Map());
-    }
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    if (!patches.get(thisClass)!.has(this.id)) {
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      patches.get(thisClass)!.set(this.id, []);
-    }
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const storedOperations = patches.get(thisClass)!.get(this.id)!;
-
-    if (Array.isArray(storedOperations)) {
-      storedOperations.push(...diff);
-
-      const nextCache =
-        cache
-          .get(this.sheetName)
-          ?.map((values) =>
-            values.id === this.id ? { ...values, ...thisValues } : values
-          ) ?? [];
-
-      if (!cachedValues) {
-        nextCache.push(thisValues);
-      }
-
-      cache.set(this.sheetName, nextCache);
-    } else {
-      logger.warn(
-        { id: this.id, sheetName: this.sheetName },
-        "Tried to save a deleted entity"
-      );
-    }
+    await updateRow(
+      thisClass,
+      this.sheetName,
+      this.id,
+      Object.fromEntries(Object.entries(this))
+    );
 
     return this;
   }
 
   async delete() {
     const thisClass = this.constructor as StaticThis<Base>;
-
-    if (!patches.has(thisClass)) {
-      patches.set(thisClass, new Map());
-    }
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    if (!patches.get(thisClass)!.has(this.id)) {
-      patches.set(thisClass, new Map());
-    }
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    patches.get(thisClass)!.set(this.id, deleteMarker);
-
-    cache.set(
-      this.sheetName,
-      cache.get(this.sheetName)?.filter((d) => d.id !== this.id) ?? []
-    );
+    await deleteRow(thisClass, this.sheetName, this.id);
   }
 
   static async all<T extends Base>(this: StaticThis<T>): Promise<T[]> {
@@ -116,28 +58,8 @@ export abstract class Base {
 
   private static async allRows<T extends Base>(cls: new () => T) {
     const sheetName = cls.prototype.sheetName;
-    if (!cache.has(sheetName)) {
-      await newrelic.startSegment(`${sheetName}.fillCache`, true, async () => {
-        newrelic.addCustomSpanAttribute("db.sheetName", sheetName);
-        const doc = await getSheet();
-        const rows = await doc.sheetsByTitle[sheetName].getRows();
-        const objectKeys = Object.keys(new cls());
-        cache.set(
-          sheetName,
-          rows.map((row) => {
-            return Object.fromEntries(
-              objectKeys.map((key) => [key, JSON.parse(row[key])])
-            );
-          })
-        );
-      });
-    }
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    return cache.get(sheetName)!.map((values) => {
-      const obj = new cls();
-      Object.assign(obj, JSON.parse(JSON.stringify(values)));
-      return obj;
-    });
+
+    return allRows(cls, sheetName);
   }
 
   toJSON() {
@@ -152,77 +74,4 @@ export abstract class Base {
       ...this,
     };
   }
-}
-
-const cache = new Map<string, Record<string, unknown>[]>();
-const patches = new Map<
-  new () => Base,
-  Map<string, Operation[] | typeof deleteMarker>
->();
-
-export async function syncCache() {
-  logger.trace("Syncing cache");
-  await newrelic.startSegment(`syncCache`, true, async () => {
-    for (const [cls, entities] of patches.entries()) {
-      const sheetName = cls.prototype.sheetName;
-      await newrelic.startSegment(`syncCahce.${sheetName}`, true, async () => {
-        const objectKeys = Object.keys(new cls());
-        const doc = await getSheet();
-        const rows = await doc.sheetsByTitle[sheetName].getRows();
-        for (const [id, operations] of entities.entries()) {
-          const row = rows.find((row) => row.id === JSON.stringify(id));
-          await newrelic.startSegment(
-            `syncCache.${sheetName}.entity`,
-            true,
-            async () => {
-              newrelic.addCustomAttribute("entity.id", id);
-              newrelic.addCustomAttribute("entity.sheetName", sheetName);
-              if (operations === deleteMarker) {
-                logger.trace({ sheetName, id }, "Deleting entity");
-                row?.delete();
-              } else {
-                const original = row
-                  ? Object.fromEntries(
-                      objectKeys.map((key) => [key, JSON.parse(row[key])])
-                    )
-                  : {};
-                try {
-                  const { newDocument } = applyPatch(
-                    original,
-                    operations,
-                    true,
-                    false
-                  );
-                  const values = Object.fromEntries(
-                    Object.entries(newDocument).map(([key, value]) => [
-                      key,
-                      JSON.stringify(value),
-                    ])
-                  );
-                  logger.trace(
-                    { sheetName, id, operations, values },
-                    "Syncing entity"
-                  );
-                  if (row) {
-                    Object.assign(row, values);
-                    await row.save();
-                  } else {
-                    await doc.sheetsByTitle[sheetName].addRow(values);
-                  }
-                } catch (err) {
-                  logger.error(
-                    { err, sheetName, id, operations, original },
-                    "Failed to apply patch"
-                  );
-                }
-              }
-            }
-          );
-        }
-      });
-    }
-  });
-  logger.trace("Synced cache");
-  patches.clear();
-  cache.clear();
 }
---
+++ packages/server/src/data/$cache.ts
@@ -0,0 +1,195 @@
+import { Operation, applyPatch, compare } from "fast-json-patch";
+import newrelic from "newrelic";
+import { logger } from "../logger";
+import { getSheet } from "./$api";
+import { Base } from "./$base";
+
+const deleteMarker = Symbol.for("delete");
+
+const cache = new Map<string, Record<string, unknown>[]>();
+const patches = new Map<
+  new () => Base,
+  Map<string, Operation[] | typeof deleteMarker>
+>();
+
+export async function deleteRow<T extends Base>(
+  cls: new () => T,
+  sheetName: string,
+  id: string
+) {
+  // ensure cache is populated
+  await allRows(cls, sheetName);
+
+  if (!patches.has(cls)) {
+    patches.set(cls, new Map());
+  }
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  if (!patches.get(cls)!.has(id)) {
+    patches.set(cls, new Map());
+  }
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  patches.get(cls)!.set(id, deleteMarker);
+
+  cache.set(sheetName, cache.get(sheetName)?.filter((d) => d.id !== id) ?? []);
+}
+
+export async function updateRow<T extends Base>(
+  cls: new () => T,
+  sheetName: string,
+  id: string,
+  values: Record<string, unknown>
+) {
+  // ensure cache is populated
+  await allRows(cls, sheetName);
+
+  const cachedValues = cache.get(sheetName)?.find((values) => values.id === id);
+
+  const diff = compare(cachedValues ?? {}, values, true);
+
+  logger.debug({ diff, cachedValues, values, sheetName }, "Storing diff");
+
+  if (!patches.has(cls)) {
+    patches.set(cls, new Map());
+  }
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  if (!patches.get(cls)!.has(id)) {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    patches.get(cls)!.set(id, []);
+  }
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const storedOperations = patches.get(cls)!.get(id)!;
+
+  if (Array.isArray(storedOperations)) {
+    storedOperations.push(...diff);
+
+    const nextCache =
+      cache
+        .get(sheetName)
+        ?.map((cachedValues) =>
+          cachedValues.id === id ? { ...cachedValues, ...values } : cachedValues
+        ) ?? [];
+
+    if (!cachedValues) {
+      nextCache.push(values);
+    }
+
+    cache.set(sheetName, nextCache);
+  } else {
+    logger.warn({ id, sheetName }, "Tried to save a deleted entity");
+  }
+}
+
+export async function allRows<T extends Base>(
+  cls: new () => T,
+  sheetName: string
+) {
+  return await newrelic.startSegment(`allRows.${sheetName}`, true, async () => {
+    newrelic.addCustomSpanAttribute("db.sheetName", sheetName);
+    if (!cache.has(sheetName)) {
+      await fillCache(cls, sheetName);
+    }
+
+    return (cache.get(sheetName) ?? []).map((values) => {
+      const obj = new cls();
+      Object.assign(obj, JSON.parse(JSON.stringify(values)));
+      return obj;
+    });
+  });
+}
+
+export async function syncCache() {
+  logger.trace("Syncing cache");
+  await newrelic.startSegment(`syncCache`, true, async () => {
+    for (const [cls, entities] of patches.entries()) {
+      const sheetName = cls.prototype.sheetName;
+      await newrelic.startSegment(`syncCache.${sheetName}`, true, async () => {
+        const objectKeys = Object.keys(new cls());
+        const doc = await getSheet();
+        const rows = await doc.sheetsByTitle[sheetName].getRows();
+        for (const [id, operations] of entities.entries()) {
+          const row = rows.find((row) => row.id === JSON.stringify(id));
+          await newrelic.startSegment(
+            `syncCache.${sheetName}.entity`,
+            true,
+            async () => {
+              newrelic.addCustomAttribute("entity.id", id);
+              newrelic.addCustomAttribute("entity.sheetName", sheetName);
+              if (operations === deleteMarker) {
+                logger.trace({ sheetName, id }, "Deleting entity");
+                row?.delete();
+              } else {
+                const original = row
+                  ? Object.fromEntries(
+                      objectKeys.map((key) => [key, JSON.parse(row[key])])
+                    )
+                  : {};
+                try {
+                  const { newDocument } = applyPatch(
+                    original,
+                    operations,
+                    true,
+                    false
+                  );
+                  const values = Object.fromEntries(
+                    Object.entries(newDocument).map(([key, value]) => [
+                      key,
+                      JSON.stringify(value),
+                    ])
+                  );
+                  logger.trace(
+                    { sheetName, id, operations, values },
+                    "Syncing entity"
+                  );
+                  if (row) {
+                    Object.assign(row, values);
+                    await row.save();
+                  } else {
+                    await doc.sheetsByTitle[sheetName].addRow(values);
+                  }
+                } catch (err) {
+                  logger.error(
+                    { err, sheetName, id, operations, original },
+                    "Failed to apply patch"
+                  );
+                }
+              }
+            }
+          );
+        }
+      });
+    }
+  });
+  logger.trace("Synced cache");
+  patches.clear();
+  cache.clear();
+}
+
+const fillingCache = new Map<string, Promise<void>>();
+async function fillCache<T extends Base>(cls: new () => T, sheetName: string) {
+  if (fillingCache.has(sheetName)) {
+    await fillingCache.get(sheetName);
+    return;
+  }
+  const promise = newrelic.startSegment(
+    `${sheetName}.fillCache`,
+    true,
+    async () => {
+      newrelic.addCustomSpanAttribute("db.sheetName", sheetName);
+      const doc = await getSheet();
+      const rows = await doc.sheetsByTitle[sheetName].getRows();
+      const objectKeys = Object.keys(new cls());
+      cache.set(
+        sheetName,
+        rows.map((row) => {
+          return Object.fromEntries(
+            objectKeys.map((key) => [key, JSON.parse(row[key])])
+          );
+        })
+      );
+      fillingCache.delete(sheetName);
+    }
+  );
+  fillingCache.set(sheetName, promise);
+  logger.trace({ sheetName }, "Filling cache");
+  await promise;
+}
