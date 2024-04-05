c774e9cfd205b64b4daf3c17c92da0c6c420b8eb
Jonathan Burke <jonathan.burke.1311@googlemail.com>
2024-04-05 09:50:10 +0200

    chore: use same port
    Signed-off-by: Jonathan Burke <jonathan.burke.1311@googlemail.com>

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,8 +1,11 @@
 import react from "@vitejs/plugin-react-swc";
+import dns from "node:dns";
 import { defineConfig } from "vite";
 import codegen from "vite-plugin-graphql-codegen";
 import { createHtmlPlugin } from "vite-plugin-html";
 import svgr from "vite-plugin-svgr";
+
+dns.setDefaultResultOrder("verbatim");
 
 // https://vitejs.dev/config/
 export default defineConfig(async ({ mode }) => ({
@@ -20,6 +23,8 @@ export default defineConfig(async ({ mode }) => ({
     sourcemap: true,
   },
   server: {
+    host: "0.0.0.0",
+    port: 5173,
     proxy: {
       "/graphql": {
         target: `http://localhost:${process.env.PORT ?? 2022}`,
@@ -30,5 +35,9 @@ export default defineConfig(async ({ mode }) => ({
         changeOrigin: true,
       },
     },
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: 5173,
   },
 }));
