4156827a3da52acb1eafe4d71174e56060403bd6
Jonathan Burke <jonathan.burke.1311@googlemail.com>
2024-06-20 09:03:46 +0200

    feat: improve image input
    Signed-off-by: Jonathan Burke <jonathan.burke.1311@googlemail.com>

--- packages/client/src/components/ImageInput.tsx
+++ packages/client/src/components/ImageInput.tsx
@@ -1,5 +1,19 @@
-import { Box, Button, Image, InputBase, InputBaseProps } from "@mantine/core";
+import {
+  Box,
+  Button,
+  Image,
+  InputBase,
+  InputBaseProps,
+  Menu,
+} from "@mantine/core";
 import { Dropzone, FileWithPath } from "@mantine/dropzone";
+import {
+  IconArrowBackUp,
+  IconCamera,
+  IconTrash,
+  IconUpload,
+} from "@tabler/icons-react";
+import { useQuery } from "@tanstack/react-query";
 import {
   InputHTMLAttributes,
   useEffect,
@@ -38,6 +52,103 @@ export function ImageInput({
       inputRef.current.value = "";
     }
   }, [file]);
+
+  const { data: devices } = useQuery(["devices"], async () => {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+
+    return devices.filter((dev) => dev.deviceId);
+  });
+
+  return (
+    <Box>
+      <InputBase
+        ref={inputRef}
+        {...props}
+        type="file"
+        onChange={(evt) => {
+          setFile(evt.currentTarget.files?.item(0) ?? undefined);
+        }}
+        style={{ display: "none" }}
+      />
+      <Menu shadow="md" withArrow position="right-start">
+        <Menu.Target>
+          <Box
+            style={{
+              width: 200,
+              height: 200,
+              borderColor: "var(--mantine-color-dimmed)",
+              borderWidth: 2,
+              borderStyle: "solid",
+              position: "relative",
+              overflow: "hidden",
+            }}
+          >
+            <Image src={url} fit="cover" height="100%" />
+            <Box
+              style={{
+                width: "100%",
+                height: "100%",
+                background:
+                  "radial-gradient(circle, rgba(0,0,0,0) 0%, rgba(0,0,0,0) 70%, rgba(0,0,0,0.7) 70%, rgba(0,0,0,0.7) 100%)",
+                position: "absolute",
+                top: 0,
+                left: 0,
+              }}
+            />
+          </Box>
+        </Menu.Target>
+
+        <Menu.Dropdown>
+          <Menu.Item
+            leftSection={<IconUpload />}
+            onClick={() => {
+              if (inputRef.current) {
+                inputRef.current.accept = "image/*";
+                inputRef.current.click();
+              }
+            }}
+          >
+            Bild hochladen
+          </Menu.Item>
+          {devices?.some((dev) => dev.kind === "videoinput") && (
+            <Menu.Item
+              leftSection={<IconCamera />}
+              onClick={() => {
+                if (inputRef.current) {
+                  inputRef.current.accept = "image/*;capture=camera";
+                  inputRef.current.click();
+                }
+              }}
+            >
+              Bild machen
+            </Menu.Item>
+          )}
+          {file && (
+            <Menu.Item
+              leftSection={<IconArrowBackUp />}
+              onClick={() => {
+                setFile(undefined);
+                setNoDefaultFallback(false);
+              }}
+            >
+              Bild zurücksetzen
+            </Menu.Item>
+          )}
+          <Menu.Item
+            color="red"
+            leftSection={<IconTrash />}
+            onClick={() => {
+              setFile(undefined);
+              setNoDefaultFallback(true);
+              onRemoveImage?.();
+            }}
+          >
+            Bild entfernen
+          </Menu.Item>
+        </Menu.Dropdown>
+      </Menu>
+    </Box>
+  );
 
   return (
     <Box>
