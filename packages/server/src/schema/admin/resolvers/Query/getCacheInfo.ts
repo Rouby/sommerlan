1f95ac2e9bb2cb4b440a6246a3c1d5b84d6cfbf4
Jonathan Burke <jonathan.burke.1311@googlemail.com>
2025-05-23 19:17:03 +0200

    fix: secure cache endpoint
    Signed-off-by: Jonathan Burke <jonathan.burke.1311@googlemail.com>

--- packages/server/src/schema/admin/resolvers/Query/getCacheInfo.ts
+++ packages/server/src/schema/admin/resolvers/Query/getCacheInfo.ts
@@ -1,8 +1,11 @@
+import { ForbiddenError } from "@casl/ability";
 import type { QueryResolvers } from "./../../../types.generated";
 export const getCacheInfo: NonNullable<QueryResolvers["getCacheInfo"]> = async (
   _parent,
   _arg,
   ctx,
 ) => {
+  ForbiddenError.from(ctx.ability).throwUnlessCan("read", "Cache");
+
   return ctx.data.getCacheInfo();
 };
