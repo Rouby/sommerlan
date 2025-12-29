5dc527ca5e4a7da4a4938e3868ba3b5319af2e92
Jonathan Burke <jonathan.burke.1311@googlemail.com>
2025-12-29 12:34:25 +0100

    Remove unused Title import from purchases route
    Signed-off-by: Jonathan Burke <jonathan.burke.1311@googlemail.com>

--- packages/client/src/routes/purchases/index.lazy.tsx
+++ packages/client/src/routes/purchases/index.lazy.tsx
@@ -1,5 +1,5 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
-import { Center, Container, Title } from "@mantine/core";
+import { Center, Container } from "@mantine/core";
 import { PurchasesList } from "../../features/Purchases/PurchasesList";
 import { CreatePurchaseForm } from "../../features/Purchases/CreatePurchaseForm";
 import { Can } from "../../components";
